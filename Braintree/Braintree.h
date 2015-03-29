55dad21d82fd055720c02ae85978e3ccf4db2da8
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-30 08:57:43 +1100

    Using braintree

--- Braintree/Braintree.h
+++ Braintree/Braintree.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char BraintreeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Braintree/PublicHeader.h>
 
-
