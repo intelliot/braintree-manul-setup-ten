93b4580cd91d305d71c6758bc522d9ffa11cf7a1
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-30 08:50:23 +1100

    Step 3

---
+++ Braintree/Braintree.h
@@ -0,0 +1,19 @@
+//
+//  Braintree.h
+//  Braintree
+//
+//  Created by Evgenii Neumerzhitckii on 30/03/2015.
+//  Copyright (c) 2015 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Braintree.
+FOUNDATION_EXPORT double BraintreeVersionNumber;
+
+//! Project version string for Braintree.
+FOUNDATION_EXPORT const unsigned char BraintreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Braintree/PublicHeader.h>
+
+
